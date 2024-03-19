c2a7bcc22d9fbb9a544ceef5109118fc64a9765b
Freddy Alvarado <falvarado@cmiconsulting.pe>
2024-03-18 21:59:32 -0500

    FAB

--- build/lib/anabook/eda.py
+++ build/lib/anabook/eda.py
@@ -80,7 +80,6 @@ def areaTrazadoBoxPlot(qcol):
   plt.rcParams['figure.figsize']=(nw,nh)
   return (nw,nh)
   
-
 # inputs, lista de campos numericos
 def boxplot2(df, inputs):
     num_inputs = len(inputs)
@@ -202,3 +201,20 @@ def describe2(df):
   plt.show()
 
   plt.rcParams['figure.figsize']=(6,6)
+
+def mKOptimo(distortions):
+  first_point = (0, distortions[0])
+  last_point = (len(distortions) - 1, distortions[-1])
+
+  m = (last_point[1] - first_point[1]) / (last_point[0] - first_point[0])
+  intercept = first_point[1] - m * first_point[0]
+
+  distances = []
+  for i in range(len(distortions)):
+    point = (i, distortions[i])
+    distance = abs(m * point[0] + intercept - point[1]) / np.sqrt(m ** 2 + 1)
+    distances.append(distance)
+
+  max_distance_index = np.argmax(distances)
+
+  return max_distance_index + 1  
